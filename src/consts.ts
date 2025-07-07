44eff9e24766480f75e39f7a3d04ab050e519706
mahesh-143 <maheshodedara13@yahoo.com>
2025-07-07 20:03:32 +0530

    feat(ui): add footer

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@ export const SITE = {
   TITLE: "Mono Lume",
   DESCRIPTION:
     "A full-stack developer with a strong focus on building fast, accessible, and maintainable web applications.",
+  EMAIL: "hi@monolume.com",
 };
